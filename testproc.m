d81f348064cf6b5cda041c770e50ffeaf5f8511b
Sharad Shanbhag <sshanbhag@neoucom.edu>
2011-01-26 18:21:56 -0500

    reads in probe data.
    need to update channels to units
    
    modified:
      testproc.m
    unknown:
      Spreadsheet_BBN_1perSec.txt
      Spreadsheet_FRA_1perSec.txt

--- testproc.m
+++ testproc.m
@@ -32,6 +32,7 @@
 %-----------------------------------------------------------
 N_HEADER_LINES = 2;
 N_COLS_PER_PROBE = 6;
+N_CHAN_PER_PROBE = 5;
 
 %-----------------------------------------------------------
 % load test.mat file containing variables:	data{}  dcount dwinfo
@@ -40,7 +41,7 @@ if ~exist('data', 'var')
 	load test.mat
 end
 
-[nrows, ncols] = size(data);
+[Drows, Dcols] = size(data);
 
 
 %-----------------------------------------------------------
@@ -50,6 +51,7 @@ end
 % these will indicate the start column for an N-Probe
 tmp = strncmpi('Probe', dwinfo.header.fields{1}, 5);
 ProbeCols = find(tmp);
+NumberOfProbes = length(ProbeCols);
 
 %-----------------------------------------------------------
 % then, find the marker col
@@ -60,11 +62,14 @@ MarkerCols = find(tmp);
 %-----------------------------------------------------------
 % find column headers
 %-----------------------------------------------------------
+% Timestamp columns
 tmp = strncmpi('Timestamp', dwinfo.header.fields{2}, 6);
 TimestampCols = find(tmp);
+% Other columns
+OtherCols = find(~tmp);
 
 %-----------------------------------------------------------
-% get Marker information
+% get Marker information and retrieve markers from data
 %-----------------------------------------------------------
 if length(MarkerCols) == 1
 	tmp = dwinfo.header.fields{2}(MarkerCols:end)
@@ -76,29 +81,80 @@ for n = 1:length(tmp)
 	MarkerNames{n} = tmp(n);
 end
 
-
-%-----------------------------------------------------------
-% Pull in Marker info
-%-----------------------------------------------------------
+% Pull in Marker data
 nMarkers = length(MarkerNames);
 mStartCol = MarkerCols(1);
 markerCount = 0;
-for l = (N_HEADER_LINES + 1):length(data)
+for l = 1:Drows
 	if isempty(data{l, mStartCol})
 		break;
 	else
 		markerCount = markerCount + 1;
-		Marker.t(l) = data{l, mStartCol};
+		Marker.t(l) = str2double(data{l, mStartCol});
 		for m = 1:nMarkers-1
 			Marker.mval{l, m} = data{l, mStartCol + m};
 		end
 	end
-	
-	
-	
 end
 
+%-----------------------------------------------------------
+% Pull in Probe Data
+%-----------------------------------------------------------
+% check # of probes
+if NumberOfProbes == 0
+	% if 0, error
+	error('%s: no probes detected in header', mfilename)
+else
+	% otherwise, build Probe data structure
+	tmpProbe = struct('C0', [], 'C1', [], 'C2', [],'C3', [], 'C4', []);
+	for n = 1:NumberOfProbes
+		Probe(n) = tmpProbe;
+	end
+end
 
+Probe2 = Probe;
+
+tic
+% loop through data lines
+for l = 1:Drows
+	% loop through probes (i.e., tetrodes)
+	for p = 1:NumberOfProbes
+		% loop through data columns for this probe
+		for c = 0:(N_CHAN_PER_PROBE-1)
+			% column in data cell array 
+			datacol = c + ProbeCols(p);
+			% if data element at line l, column datacol is not empty,
+			% assign the value (convert to double from string) to the
+			% appropriate channel in the probe struct array
+			if ~isempty(data{l, datacol})
+				Probe(p).(['C' num2str(c)])(l) = str2double(data{l, datacol});
+			end
+		end % end c
+	end % end p
+end % end l
+method1 = toc
+
+
+tic
+% loop through data lines
+for l = 1:Drows
+	% loop through probes (i.e., tetrodes)
+	for p = 1:NumberOfProbes
+		% loop through data columns for this probe
+		for c = 0:(N_CHAN_PER_PROBE-1)
+			% column in data cell array 
+			datacol = c + ProbeCols(p);
+			
+			% if data element at line l, column datacol is not empty,
+			% assign the value (convert to double from string) to the
+			% appropriate channel in the probe struct array
+			if ~isempty(data{l, datacol})
+				Probe(p).(['C' num2str(c)])(l) = str2double(data{l, datacol});
+			end
+		end % end c
+	end % end p
+end % end l
+method2 = toc
 
 
 
