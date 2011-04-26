751e0745b09e2eb38aa0e8dddc86f5323a73e985
Sharad Shanbhag <sshanbhag@neoucom.edu>
2011-04-26 18:18:09 -0400

    moved txtnum for Marker wav files into loadDWStimData

--- testreaddw.m
+++ testreaddw.m
@@ -33,32 +33,32 @@ end
 % 	 .
 % 	 .
 % 	 etc. ...
+% 
+% 
+% % copy plx data to plx
+% plx = rawstim.plxdata;
+% % add event timestamps to event_ts field in plx as cell
+% plx.event_ts = {plx.event_ts}
+% % specify event channel, counts, etc.
+% plx.noof_event_chans = 1;
+% plx.event_chan_id = [258];
+% plx.event_ts_counts = [length(plx.event_ts{1})];
+% 
+% 
+% % create textnum field in rawstim.marker struct to hold stim id numbers
+% rawstim.marker.textnum = zeros(size(rawstim.marker.value));
+% for n = 1:length(stimdata)
+% 	stimindex = stimdata(n).indices;
+% 	rawstim.marker.textnum(stimindex) = stim_idnums(n) * ones(size(stimindex));
+% end
+% 
+% vars.values = [ rawstim.marker.textnum rawstim.marker.value ];
+% vars.names = {'wavfile_id' 'atten_dB'};
+% vars.description = 'Test Stimuli';
 
+spikes = PLX2SpikeCount(rawstim.plxdata,'var_matrix','variables',rawstim.vars,'end',50e-3);
 
-% copy plx data to plx
-plx = rawstim.plxdata;
-% add event timestamps to event_ts field in plx as cell
-plx.event_ts = {plx.event_ts}
-% specify event channel, counts, etc.
-plx.noof_event_chans = 1;
-plx.event_chan_id = [258];
-plx.event_ts_counts = [length(plx.event_ts{1})];
-
-
-% create textnum field in rawstim.marker struct to hold stim id numbers
-rawstim.marker.textnum = zeros(size(rawstim.marker.value));
-for n = 1:length(stimdata)
-	stimindex = stimdata(n).indices;
-	rawstim.marker.textnum(stimindex) = stim_idnums(n) * ones(size(stimindex));
-end
-
-vars.values = [ rawstim.marker.textnum rawstim.marker.value ];
-vars.names = {'wavfile_id' 'atten_dB'};
-vars.description = 'Test Stimuli';
-
-spikes = PLX2SpikeCount(plx,'var_matrix','variables',vars,'end',50e-3);
-
-display_genData(spikes,'xdim',2,'axesdimlist',1,'conf95','normalize','xvalstep',1,'minval',0,'title', vars.description);
+display_genData(spikes, 'xdim',2, 'axesdimlist',1, 'conf95','normalize', 'xvalstep',1, 'minval',0,'title', rawstim.vars.description);
 thr_conf95_excite = spikecount2thr(spikes,'increase','dimname','atten_dB','conf95',2,'interpolate')
 thr_conf95_inhibit = spikecount2thr(spikes,'decrease','dimname','atten_dB','conf95',2,'interpolate')
-display_genData(thr_conf95_excite,'xdim',2,'valuelinestyle','o-','title', vars.description);
+display_genData(thr_conf95_excite,'xdim',2,'valuelinestyle','o-','title', rawstim.vars.description);
