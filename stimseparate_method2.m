a081eaf0651eea0264060b9fe5b1fe6572d96711
Sharad Shanbhag <sshanbhag@neoucom.edu>
2011-06-23 18:23:13 -0400

    getting closer, need to finish stimulus struct stuff then on to timestamps etc

--- stimseparate_method2.m
+++ stimseparate_method2.m
@@ -12,11 +12,14 @@ Nmarkers = length(Marker.Timestamp);
 % build a global stimulus tag cell array, Tags
 % and an vector of strings that will be compared
 %-----------------------------------------------------------------------------
-% preallocate Tags cell array
+% preallocate Tags cell array to hold data from all Marker stimulus "tags"
+% (a.k.a. stimulus parameters) and TagStrings{} cell vector to store
+% strings made up from each Marker's Tags (for use in searching, see below)
 Tags = cell(Nmarkers, 2*NTAGS_PER_CHANNEL);
 TagStrings = cell(Nmarkers, 1);
 
-
+% ctxt will be used to select Right and Left channels in the
+% STIMULUS_SEARCH_TAGS{} strings
 ctxt = 'RL';
 
 % loop through Markers
@@ -29,16 +32,17 @@ for m = 1:Nmarkers
 			offset = (c-1)*NTAGS_PER_CHANNEL;
 
 			% need to check if each specific tag is in a cell or vector in order
-			% to address the tag appropriately (using {} for cell, () for vector)
-		
+			% to address the tag appropriately (using {} for cell, () for
+			% vector)
 			if iscell(Marker.([STIMULUS_SEARCH_TAGS{t} ctxt(c)]))
 				Tags{m, t + offset} = Marker.([STIMULUS_SEARCH_TAGS{t} ctxt(c)]){m};
 			else
 				Tags{m, t + offset} = Marker.([STIMULUS_SEARCH_TAGS{t} ctxt(c)])(m);
 			end
-		end
-	end
-end
+			
+		end % END OF c (channel) LOOP
+	end	% END OF t (tag) LOOP
+end	% END OF m (marker) LOOP
 
 %-----------------------------------------------------------------------------
 % Now we have a cell array that has a mix of text and numeric fields.
@@ -61,10 +65,55 @@ TagStrings = cell2str(Tags);
 %-----------------------------------------------------------------------------
 % Determine Stimulus Type
 %-----------------------------------------------------------------------------
+% preallocate some storage structures
+BG = struct('indices', [], 'count', 0);
+Stimulus = repmat( cell2struct(STIMULUS_STRUCT_FIELDS, STIMULUS_STRUCT_FIELDS, 2), Nunique, 1);
+
 for n = 1:Nunique
+	
+	% extract type of stimulus from Marker Data
+	% we'll use the first instance of the stimulus, knowing that
+	% if the sorting method to determine unique stimuli from above
+	% messes up, this will also be fatally flawed...  
+	
+	% get index to first instance in Marker vectors
+	mIndex = uniqueIndices{n}(1);
+	rtype = Marker.StimulusTypeR{1, 1};
+	ltype = Marker.StimulusTypeL{1, 1};
+	
+	% first, determine if this is a background trial (no sound played)
+	% as well as the channel for the sound stimulus (left, right, both)
+	if strcmp(rtype, 'NO_SOUND') && strcmp(ltype, 'NO_SOUND')
+		% if both of the type tags are 'NO_SOUND', then 
+		% this is a background trial
+		% save Marker indices in the BG (Background) struct and increment counter
+		BG.count = BG.count + 1;
+		BG.indices{BG.count} = unique_indices{n};
+		Stimulus(n).Type = {'BACKGROUND'};		
+		Stimulus(n).Channel = 'B';
+		
+	elseif strcmp(rtype, 'NO_SOUND') && ~strcmp(ltype, 'NO_SOUND')
+		% sound is on Left Channel
+		Stimulus(n).Channel = 'L';
+		Stimulus(n).Type = {ltype};
+		
+	elseif ~strcmp(rtype, 'NO_SOUND') && strcmp(ltype, 'NO_SOUND')
+		% sound is on Right Channel
+		Stimulus(n).Channel = 'R';
+		Stimulus(n).Type = {rtype};
+	else
+		% Both channels
+		Stimulus(n).Channel = 'B';
+		Stimulus(n).Type = {rtype ltype};
+	end
+	
+	Stimulus(n).Indices = uniqueIndices{n};
+	Stimulus(n).tagstring = uniqueText{n};
+	
+	% Now copy over the Marker tags
 	
 end
 
 %-----------------------------------------------------------------------------
-%
+% Now, for each stimulus, figure out what variables
 %-----------------------------------------------------------------------------
