1a6221f57989b0aed3156cde808e48d85646f094
Sharad Shanbhag <sshanbhag@neoucom.edu>
2011-07-05 14:05:10 -0400

    mostly working, need to work on display of data

--- plotRasters.m
+++ plotRasters.m
@@ -1,5 +1,5 @@
 %------------------------------------------------------------------------
-% 
+% plotRasters
 %------------------------------------------------------------------------
 % 
 %------------------------------------------------------------------------
@@ -33,50 +33,107 @@ DataWaveDefaults;
 %-----------------------------------------------------------
 load loadDWfile_debug.mat
 
-Nstimuli = length(Stimulus);
 
 %-----------------------------------------------------------
-%
+% for test data, all stimuli are tones, so this next section
+% is not strictly necessary.  however, for future use with
+% intermingled stimuli, this will show how to pull out stimuli
+% of specific types
 %-----------------------------------------------------------
-for s = 1:Nstimuli
+toneIndex = 0;
+for s = 1:length(Stimulus)
 	disp(sprintf('Stimulus(%d):\n\t type:\t %s', s, Stimulus(s).Type{1}))
 	disp(sprintf('\t channel:\t %s', Stimulus(s).Channel))
+	% save tone stimulus information in StimList struct array
+	if strcmp(Stimulus(s).Type, 'TONE')
+		toneIndex = toneIndex + 1;
+		StimList(toneIndex) = Stimulus(s);
+	end
 end
 
+% get number of stimuli
+Nstimuli = length(StimList);
 
 % find ranges of tone stimuli
-
 for s = 1:Nstimuli
-	if strcmp(Stimulus(s).Type, 'TONE')
-		ToneFreq(s) = Stimulus(s).Var(1).values;
-	end
+	ToneFreq(s) = StimList(s).Var(1).values;
 end
 
 % find ranges of attenuation
 for s = 1:Nstimuli
 	% R channel attenuation
-	AttenR{s} = Stimulus(s).RAttenVals;
-	AttenR_indices{s} = Stimulus(s).RAttenIndices;
+	AttenR{s} = StimList(s).RAttenVals;
+	AttenR_indices{s} = StimList(s).RAttenIndices;
 end
 
 
+%----------------------------------------------------------------------
+%----------------------------------------------------------------------
 % build up Spikes cell array to hold spike time information for 
-
+%----------------------------------------------------------------------
+%----------------------------------------------------------------------
+% 
+% Within each Stimulus (or, StimList in this case) struct element, there are 
+% a few key elements for organizing the spikes.
+% 
+% {R/L}AttenIndices{} contains a list of indices to Stimulus vectors that correspond
+% to attenuation values given in {R/L}AttenVals
+% 
+% for example, 
+% 	
+% 	The vector at Stimulus(1).RAttenIndices{1} correspondes to a list of 
+% 	indices for Stimulus(1) where Stimulus(1)RAttenVals(1) was the 
+% 	attenuation setting.  
+% 	
+% 	So, in order to get the spikestimes corresponding to Stimulus(1) and the 
+% 	attenuation value of Stimulus(1).RAttenVals(1), we would use the following
+% 	code:
+% 	
+% 	spikes = Stimulus(1).Spiketimes(<unit number>, Stimulus(1).RAttenIndices{1})
+% 		(*** parentheses notation   ^     and                                   ^)
+% 		
+% 	Which returns a cell array of spike time stamp vectors:
+% 	
+% 	>> Stimulus(1).Spiketimes(1, Stimulus(1).RAttenIndices{1})
+% 
+% 		ans = 
+% 
+% 		 []    [1x15 double]    [1x14 double]
+% 
+% 	To obtain individual vectors of spike times, use the following notation
+% 	(pulling in the second timestamp vector because the first one is empty):
+% 	
+% 	spikes = Stimulus(1).Spiketimes{1, Stimulus(1).RAttenIndices{1}(2)}
+% 	
+% 	>>	spikes = Stimulus(1).Spiketimes{1, Stimulus(1).RAttenIndices{1}(2)}
+% 
+% 		spikes =
+% 
+% 		Columns 1 through 10
+% 
+% 		  2868100     2869800     2870000     2870100     2870300     2870500     2870700     2870800     2871000     2871100
+% 
+% 		Columns 11 through 15
+% 
+% 		  2871200     2871300     2871500     2871600     2871700
+%----------------------------------------------------------------------
 % loop through units
 for unitindex = 1:D.Info.Nunits
 	% loop through frequencies (for tone stimuli)
 	for freqindex = 1:Nstimuli
 		% loop through different attenuation values
-		for attenindex = 1:length(Stimulus(freqindex).RAttenVals)
-			% loop through individual sweeps for this attenuation setting
+		for attenindex = 1:length(StimList(freqindex).RAttenVals)
+			% clear the tmpcell variable to avoid confusing results
 			clear tmpcell;
-			for sweepindex = 1:length(Stimulus(freqindex).RAttenIndices{attenindex})
-				spikecol = Stimulus(freqindex).RAttenIndices{attenindex}(sweepindex);
-				tmpcell{sweepindex} = Stimulus(freqindex).Spiketimes{unitindex, spikecol};
-				%%% should probably subtract off Spikestart(sweepindex) to get
-				%%% times relative to start of sweep %%%%%%%%%
+			
+			% loop through individual sweeps for this attenuation setting
+			for sweepindex = 1:length(StimList(freqindex).RAttenIndices{attenindex})
+				spikecol = StimList(freqindex).RAttenIndices{attenindex}(sweepindex);
+				tmpcell{sweepindex} = StimList(freqindex).Spiketimes{unitindex, spikecol};
+				% subtract off Spikestart(sweepindex) to get times relative to 
+				% start of sweep and then convert from usec to msec
 				if ~isempty(tmpcell{sweepindex})
-					tmpcell{sweepindex} = tmpcell{sweepindex} -  Stimulus(freqindex).Sweepstart(spikecol);
+					tmpcell{sweepindex} = tmpcell{sweepindex} -  StimList(freqindex).Sweepstart(spikecol);
 					tmpcell{sweepindex} = 0.001 * tmpcell{sweepindex};
 				end
 			end
@@ -85,22 +142,21 @@ for unitindex = 1:D.Info.Nunits
 	end	% end of FREQINDEX
 end	% end of UNITINDEX
 
-return
 
 % plot a raster for each unit
 
-unit = 1;
+unit = 3;
 H = figure(1);
 
 for freq = 1:Nstimuli
 	
-	for atten = 1:length(Stimulus(freqindex).RAttenVals)
-		clear spiketimes_ms;
-		for n = 1:length(Spikes{unit, freq, atten})
-			spiketimes_ms{n} = 0.001 * Spikes{unit, freq, atten}(n);
-		end
-		rasterplot(spiketimes_ms, [0 1000], H);
+	for atten = 1:length(StimList(freq).RAttenVals)
+		rasterplot(Spikes{unit, freq, atten}, [0 1000], H);
 		
+		titlestr = sprintf('Unit %d: Atten = %d Freq = %.2f', ...
+						unit, StimList(freq).RAttenVals(atten), StimList(freq).Var(1).values(1));
+		title(titlestr)
+
 		pause
 	end
 end
