a7880e5a58334cfdd3e7f9d40086308d276108b8
Sharad Shanbhag <sshanbhag@neomed.edu>
2012-06-05 12:10:13 -0400

    modified:   +DW/@DWdata/buildStimuliFromMarkers.m 	- resolved Marker struct -> object issue 	- working on Stimulus (struct) -> Stimuli (array of Stimulus objects) conversion

--- +DW/@DWdata/buildStimuliFromMarkers.m
+++ +DW/@DWdata/buildStimuliFromMarkers.m
@@ -81,21 +81,10 @@ for m = 1:obj.Nmarkers
 		for c = 1:N_CHANNELS
 			% offset to handle R vs L channel data
 			offset = (c-1)*NTAGS_PER_CHANNEL;
-
-			% need to check if each specific tag is in a cell or vector in order
-			% to address the tag appropriately (using {} for cell, () for
-			% vector)
-			if iscell(Marker.([STIMULUS_SEARCH_TAGS{t} ctxt(c)]))
-				Tags{m, t + offset} = Marker.([STIMULUS_SEARCH_TAGS{t} ctxt(c)]){m};
-			else
-				Tags{m, t + offset} = Marker.([STIMULUS_SEARCH_TAGS{t} ctxt(c)])(m);
-			end
-			
+			Tags{m, t + offset} = Marker(m).([STIMULUS_SEARCH_TAGS{t} ctxt(c)]);
 		end % END OF c (channel) LOOP
 	end	% END OF t (tag) LOOP
 end	% END OF m (marker) LOOP
-
-return
 
 %-----------------------------------------------------------------------------
 % Now we have a cell array that has a mix of text and numeric fields.
@@ -104,7 +93,6 @@ return
 %			stored in TagStrings{}
 %	(2)	use findUniqueText function to locate unique stimuli
 %-----------------------------------------------------------------------------
-
 % perform conversion to strings.
 TagStrings = cell2str(Tags);
 
@@ -113,22 +101,23 @@ TagStrings = cell2str(Tags);
 % stimulus parameters.
 [uniqueText, uniqueIndices, Nunique] = findUniqueText(TagStrings);
 
+keyboard
 
 %-----------------------------------------------------------------------------
 % Determine Stimulus Type
 %-----------------------------------------------------------------------------
 % preallocate some storage structures
 BG = struct('indices', [], 'count', 0);
-Stimulus = repmat( cell2struct(STIMULUS_STRUCT_FIELDS, STIMULUS_STRUCT_FIELDS, 2), Nunique, 1);
-Nstimuli = length(Stimulus);
+Stimuli = repmat( cell2struct(STIMULUS_STRUCT_FIELDS, STIMULUS_STRUCT_FIELDS, 2), Nunique, 1);
+Nstimuli = length(Stimuli);
 
 for n = 1:Nunique
-	
+	%------------------------------------------------------------------
 	% extract type of stimulus from Marker Data
-	% we'll use the first instance of the stimulus, knowing that
+	% use the first instance of the stimulus, knowing that
 	% if the sorting method to determine unique stimuli from above
 	% messes up, this will also be fatally flawed...  
-	
+	%------------------------------------------------------------------
 	% get index to first instance in Marker vectors
 	mIndex = uniqueIndices{n}(1);
 	rtype = Marker.StimulusTypeR{1, 1};
@@ -142,35 +131,35 @@ for n = 1:Nunique
 		% save Marker indices in the BG (Background) struct and increment counter
 		BG.count = BG.count + 1;
 		BG.indices{BG.count} = unique_indices{n};
-		Stimulus(n).Type = {'BACKGROUND'};		
-		Stimulus(n).Channel = 'B';
+		Stimuli(n).Type = {'BACKGROUND'};		
+		Stimuli(n).Channel = 'B';
 		
 	elseif strcmp(rtype, 'NO_SOUND') && ~strcmp(ltype, 'NO_SOUND')
 		% sound is on Left Channel
-		Stimulus(n).Channel = 'L';
-		Stimulus(n).Type = {ltype};
+		Stimuli(n).Channel = 'L';
+		Stimuli(n).Type = {ltype};
 		
 	elseif ~strcmp(rtype, 'NO_SOUND') && strcmp(ltype, 'NO_SOUND')
 		% sound is on Right Channel
-		Stimulus(n).Channel = 'R';
-		Stimulus(n).Type = {rtype};
+		Stimuli(n).Channel = 'R';
+		Stimuli(n).Type = {rtype};
 	else
 		% Both channels
-		Stimulus(n).Channel = 'B';
-		Stimulus(n).Type = {rtype ltype};
+		Stimuli(n).Channel = 'B';
+		Stimuli(n).Type = {rtype ltype};
 	end
 	
-	Stimulus(n).Indices = uniqueIndices{n};
-	Stimulus(n).Tagstring = uniqueText{n};
-	Stimulus(n).Nreps = length(uniqueIndices{n});
+	Stimuli(n).Indices = uniqueIndices{n};
+	Stimuli(n).Tagstring = uniqueText{n};
+	Stimuli(n).Nreps = length(uniqueIndices{n});
 	
 	% Now copy over the Marker tags and values
 	for f = 1:length(MARKER_TAGS)
 		if iscell(Marker.(MARKER_TAGS{f}))
-			Stimulus(n).(MARKER_TAGS{f}) = ...
+			Stimuli(n).(MARKER_TAGS{f}) = ...
 										Marker.(MARKER_TAGS{f}){uniqueIndices{n}};
 		else
-			Stimulus(n).(MARKER_TAGS{f}) = ...
+			Stimuli(n).(MARKER_TAGS{f}) = ...
 										Marker.(MARKER_TAGS{f})(uniqueIndices{n});
 		end
 	end
@@ -178,7 +167,7 @@ for n = 1:Nunique
 end
 
 %-----------------------------------------------------------------------------
-% this is a bit of a kludge - the Timestamp field in Stimulus only has the 
+% this is a bit of a kludge - the Timestamp field in Stimuli only has the 
 % first timestamp, whereas we need all timestamps for each stimulus type in
 % 
 % This information will be stored in the Sweepstart(1:Nreps) and
@@ -198,26 +187,26 @@ if max_timestamp_difference <= 0
 						mfilename, max_timestamp_difference);
 end
 
-% then get the timestamps for this Stimulus from the Marker structure
+% then get the timestamps for this Stimuli from the Marker structure
 for s = 1:Nstimuli
-	Stimulus(s).Sweepstart = marker_timestamp_vals(Stimulus(s).Indices);
-	Stimulus(s).Sweepend = zeros(size(Stimulus(s).Sweepstart));
+	Stimuli(s).Sweepstart = marker_timestamp_vals(Stimuli(s).Indices);
+	Stimuli(s).Sweepend = zeros(size(Stimuli(s).Sweepstart));
 	% Perform a switcheroo on the Timestamp field.
-	Stimulus(s).FirstTimestamp = str2double(Stimulus(s).Timestamp);
-	Stimulus(s).Timestamp = Stimulus(s).Sweepstart;
+	Stimuli(s).FirstTimestamp = str2double(Stimuli(s).Timestamp);
+	Stimuli(s).Timestamp = Stimuli(s).Sweepstart;
 	
 	% and determine end time for sweep
-	for n = 1:length(Stimulus(s).Indices)
-		start_index = Stimulus(s).Indices(n);
+	for n = 1:length(Stimuli(s).Indices)
+		start_index = Stimuli(s).Indices(n);
 		if start_index ~= Marker.Nmarkers
 			% Set the Sweep end time to the Timestamp of the next marker
-			Stimulus(s).Sweepend(n) = marker_timestamp_vals(start_index + 1);
+			Stimuli(s).Sweepend(n) = marker_timestamp_vals(start_index + 1);
 			
 		else
 			% if the index for this stimulus (into the Markers arrays) is
 			% equal to the total # of markers, then this is the last marker and
 			% the Sweepend time will need to be computed
-			Stimulus(s).Sweepend(n) = marker_timestamp_vals(start_index) + max_timestamp_difference;
+			Stimuli(s).Sweepend(n) = marker_timestamp_vals(start_index) + max_timestamp_difference;
 
 		end
 		
@@ -284,14 +273,16 @@ for s = 1:Nstimuli
 	% store valid postTimes
 	postTimes(valid_post) = marker_timestamp_vals(postIndices(valid_post));
 	
-	% assign to Stimulus struct
-	Stimulus(s).PreSweep = preTimes;
-	Stimulus(s).PostSweep = postTimes;
+	% assign to Stimuli struct
+	Stimuli(s).PreSweep = preTimes;
+	Stimuli(s).PostSweep = postTimes;
 end
 
 %-----------------------------------------------------------------------------
+%-----------------------------------------------------------------------------
 % Now, for each stimulus, figure out what variables are varying.  or were
 % varied.  or the variety of varying variables.
+%-----------------------------------------------------------------------------
 %-----------------------------------------------------------------------------
 
 %-----------------------------------------------------------------------------
@@ -304,22 +295,20 @@ end
 %	5 different wav files randomly presented - this needs to be detected
 %	and accounted for.
 %-----------------------------------------------------------------------------	
-
 for s = 1:Nstimuli
 	% check the stimulus Type - it is stored as a string within a cell, so 
 	% it must be addressed using Type{} format.  
-	switch Stimulus(s).Type{1}
+	switch Stimuli(s).Type{1}
 		case 'TONE'
-			Stimulus(s).Var = assignVarTags(Stimulus(s), TONE_VAR_TAGS);
+			Stimuli(s).Var = assignVarTags(Stimuli(s), TONE_VAR_TAGS);
 		case	'NOISE'
-			Stimulus(s).Var = assignVarTags(Stimulus(s), NOISE_VAR_TAGS);
+			Stimuli(s).Var = assignVarTags(Stimuli(s), NOISE_VAR_TAGS);
 		case 	'WAVFILE'
-			Stimulus(s).Var = assignVarTags(Stimulus(s), WAV_VAR_TAGS);
+			Stimuli(s).Var = assignVarTags(Stimuli(s), WAV_VAR_TAGS);
 		otherwise
-			error('%s: UNKNOWN STIMULUS TYPE: %s ', mfilename, Stimulus(s).Type{1}); 
+			error('%s: UNKNOWN STIMULUS TYPE: %s ', mfilename, Stimuli(s).Type{1}); 
 	end
 end
-
 
 %------------------------------------------------------------------------
 % Determine attenuation values for each stimulus
@@ -327,24 +316,24 @@ end
 for s = 1:Nstimuli
 	% get attenuation setting for Left channel if stimulus is played over
 	% Both or Left only channel
-	if strcmp(Stimulus(s).Channel, 'B') || strcmp(Stimulus(s).Channel, 'L')
-		[Stimulus(s).LAttenVals, Stimulus(s).LAttenIndices, tmpn] = ...
-													findUniqueValues(Stimulus(s).AttenuationL);
+	if strcmp(Stimuli(s).Channel, 'B') || strcmp(Stimuli(s).Channel, 'L')
+		[Stimuli(s).LAttenVals, Stimuli(s).LAttenIndices, tmpn] = ...
+													findUniqueValues(Stimuli(s).AttenuationL);
 	else
 		% otherwise, set to empty values
-		Stimulus(s).LAttenVals = [];
-		Stimulus(s).LAttenIndices = {};
+		Stimuli(s).LAttenVals = [];
+		Stimuli(s).LAttenIndices = {};
 	end
 	
 	% get attenuation setting for Right channel if stimulus is played over
 	% Both or Right only channel
-	if strcmp(Stimulus(s).Channel, 'B') || strcmp(Stimulus(s).Channel, 'R')
-		[Stimulus(s).RAttenVals, Stimulus(s).RAttenIndices, tmpn] = ...
-													findUniqueValues(Stimulus(s).AttenuationR);
+	if strcmp(Stimuli(s).Channel, 'B') || strcmp(Stimuli(s).Channel, 'R')
+		[Stimuli(s).RAttenVals, Stimuli(s).RAttenIndices, tmpn] = ...
+													findUniqueValues(Stimuli(s).AttenuationR);
 	else
 		% otherwise, set to empty values
-		Stimulus(s).LAttenVals = [];
-		Stimulus(s).LAttenIndices = {};
+		Stimuli(s).LAttenVals = [];
+		Stimuli(s).LAttenIndices = {};
 	end
 end
 
@@ -358,10 +347,10 @@ Nunits = Data.Info.Nunits;
 % loop through stimuli
 for s = 1:Nstimuli
 	% allocate the cell array to store valid spike times for each unit
-	Stimulus(s).Spiketimes = cell(Nunits, Stimulus(s).Nreps);
+	Stimuli(s).Spiketimes = cell(Nunits, Stimuli(s).Nreps);
 
 	% loop through sweeps
-	for r = 1:Stimulus(s).Nreps
+	for r = 1:Stimuli(s).Nreps
 	
 		% loop through the units
 		for u = 1:Nunits
@@ -374,51 +363,51 @@ for s = 1:Nstimuli
 			% Spikes during this stimulus
 			%----------------------------------------------------------------			
 			% spiketimes greater than sweep start
-			startT = (UnitData(u).timestamp >= Stimulus(s).Sweepstart(r));
+			startT = (UnitData(u).timestamp >= Stimuli(s).Sweepstart(r));
 			% spiketimes less than sweep end
-			endT	= (UnitData(u).timestamp < Stimulus(s).Sweepend(r));
+			endT	= (UnitData(u).timestamp < Stimuli(s).Sweepend(r));
 			% AND the two lists
 			valid_times_list = startT & endT;
 			% store the values if spikes were found, 
 			if any(valid_times_list)
-				Stimulus(s).Spiketimes{u, r} = UnitData(u).timestamp(valid_times_list);
+				Stimuli(s).Spiketimes{u, r} = UnitData(u).timestamp(valid_times_list);
 			else
 				%otherwise, set to empty array
-				Stimulus(s).Spiketimes{u, r} = [];
+				Stimuli(s).Spiketimes{u, r} = [];
 			end
 			
 			%----------------------------------------------------------------
 			% Spikes before this stimulus
 			%----------------------------------------------------------------			
 			% spiketimes greater than pre-sweep start
-			startT = (UnitData(u).timestamp > Stimulus(s).PreSweep(r));
+			startT = (UnitData(u).timestamp > Stimuli(s).PreSweep(r));
 			% spiketimes less than sweep end
-			endT	= (UnitData(u).timestamp < Stimulus(s).Sweepstart(r));
+			endT	= (UnitData(u).timestamp < Stimuli(s).Sweepstart(r));
 			% AND the two lists
 			valid_times_list = startT & endT;
 			% store the values if spikes were found, 
 			if any(valid_times_list)
-				Stimulus(s).Spiketimes_PreStim{u, r} = UnitData(u).timestamp(valid_times_list);
+				Stimuli(s).Spiketimes_PreStim{u, r} = UnitData(u).timestamp(valid_times_list);
 			else
 				%otherwise, set to empty array
-				Stimulus(s).Spiketimes_PreStim{u, r} = [];
+				Stimuli(s).Spiketimes_PreStim{u, r} = [];
 			end
 
 			%----------------------------------------------------------------
 			% Spikes after this stimulus
 			%----------------------------------------------------------------			
 			% spiketimes greater than sweep end
-			startT = (UnitData(u).timestamp > Stimulus(s).Sweepend(r));
+			startT = (UnitData(u).timestamp > Stimuli(s).Sweepend(r));
 			% spiketimes less than post-sweep end
-			endT	= (UnitData(u).timestamp < Stimulus(s).PostSweep(r));
+			endT	= (UnitData(u).timestamp < Stimuli(s).PostSweep(r));
 			% AND the two lists
 			valid_times_list = startT & endT;
 			% store the values if spikes were found, 
 			if any(valid_times_list)
-				Stimulus(s).Spiketimes_PostStim{u, r} = UnitData(u).timestamp(valid_times_list);
+				Stimuli(s).Spiketimes_PostStim{u, r} = UnitData(u).timestamp(valid_times_list);
 			else
 				%otherwise, set to empty array
-				Stimulus(s).Spiketimes_PostStim{u, r} = [];
+				Stimuli(s).Spiketimes_PostStim{u, r} = [];
 			end			
 			
 		end	% end of U loop
