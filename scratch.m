56af36c5b2c285e9de4fe5584f741736d33418fd
Sharad Shanbhag <sshanbhag@neomed.edu>
2012-12-13 13:44:42 -0500

    first git commit

--- scratch.m
+++ scratch.m
@@ -2,14 +2,14 @@
 % working on object files
 %-----------------------------------------------------------------------------
 
-% clear all
-% 
-% % create instance
-% d = DW.DWdata([pwd filesep 'BATtest.txt']);
-% 
-% % load Markers - this will read in raw data from BATtest.txt and parse the
-% % marker information
-% d.loadData
+clear all
+
+% create instance
+d = DW.DWdata([pwd filesep 'BATtest.txt']);
+
+% load Markers - this will read in raw data from BATtest.txt and parse the
+% marker information
+d.loadData
 
 d.buildStimuliFromMarkers
 
